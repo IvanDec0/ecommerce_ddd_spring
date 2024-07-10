8a334b9e59da0a766130418da1e2fe92396d453a
Ivan <ivandecima.facultad@gmail.com>
2024-07-10 14:35:13 -0300

    project structure started

---
+++ src/main/java/com/ecommerce_learning/ecommerce_learning/EcommerceLearningApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce_learning.ecommerce_learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceLearningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ecommerce_learning/ecommerce_learning/EcommerceLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce_learning.ecommerce_learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
